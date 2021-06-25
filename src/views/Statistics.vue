5a7580d9d3d53593291eced8634a92b221a138a8
xuhao <bingoxuhao@gmail.com>
2021-06-25 11:56:23 +0800

    重复 3 次布局样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Statistics.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Statistics.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,15 @@ name: "Statistics",
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
