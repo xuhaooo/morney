8a2faaef67c2930cce47b3911dbe222c3d4f275e
xuhao <bingoxuhao@gmail.com>
2021-06-28 21:11:57 +0800

    基本完成 Tags 模块

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,29 +1,41 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in dataSource" :key="tag"
           :class="{selected: selectedTags.indexOf(tag)>=0}"
-          @click="toggle(tag)">{{tag}}</li>
+          @click="toggle(tag)">{{ tag }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
+
 @Component
-export default class Tags extends Vue{
-  @Prop() dataSource: string[] | undefined;
+export default class Tags extends Vue {
+  @Prop() readonly dataSource: string[] | undefined;
   selectedTags: string[] = [];
-  toggle(tag: string){
-    const index = this.selectedTags.indexOf(tag)
-    if(index>=0){
-      this.selectedTags.splice(index, 1)
-    }else {
-      this.selectedTags.push(tag)
+
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+  }
+
+  create() {
+    const name = window.prompt('请输入标签名');
+    if (name === '') {
+      window.alert('标签名不能为空');
+    } else if (this.dataSource) {
+      this.$emit('update:dataSource', [...this.dataSource, name]);
     }
   }
 }
@@ -36,9 +48,11 @@ export default class Tags extends Vue{
   flex-grow: 1;
   display: flex;
   flex-direction: column-reverse;
+
   > .current {
     display: flex;
     flex-wrap: wrap;
+
     > li {
       $bg: #d9d9d9;
       background: $bg;
@@ -49,14 +63,17 @@ export default class Tags extends Vue{
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
+
       &.selected {
         background: darken($bg, 50%);
         color: #fff;
       }
     }
   }
+
   > .new {
     padding-top: 16px;
+
     button {
       background: transparent;
       border: none;
