eaad2befcb2f67f2ec0dc449322a15f8f07a9d5f
xuhao <bingoxuhao@gmail.com>
2021-06-28 09:25:06 +0800

    添加 Money 布局以及全局样式和全局变量

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,14 @@
 </template>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-body {
-  line-height: 1.5;
-}
-a {
-  text-decoration: none;
-  color: inherit;
-}
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #333;
+  font-family: $font-hei;
+  line-height: 1.5;
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -24,6 +24,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   flex-direction: row;
@@ -42,7 +43,7 @@ nav {
     }
   }
   > .item.selected {
-    color: red;
+    color: $color-highlight;
   }
 }
 </style>
