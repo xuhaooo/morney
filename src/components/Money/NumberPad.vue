6f1a0e18b8a734d79a6d0e53e16ea7281e9da291
xuhao <bingoxuhao@gmail.com>
2021-06-29 09:31:00 +0800

    收集 4 个组件的数据到 record；NumberPad 中运行时类型检测 Number bug 未解决

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -13,7 +13,7 @@
       <button @click="inputContent">7</button>
       <button @click="inputContent">8</button>
       <button @click="inputContent">9</button>
-      <button class="ok">OK</button>
+      <button @click="ok" class="ok">OK</button>
       <button @click="inputContent" class="zero">0</button>
       <button @click="inputContent">.</button>
     </div>
@@ -22,11 +22,12 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class NumberPad extends Vue {
-  output = '0';
+  @Prop() readonly value!: number;
+  output = this.value.toString();
   inputContent(event: MouseEvent) {
     const button = (event.target as HTMLButtonElement);
     const input = button.textContent!;
@@ -52,9 +53,9 @@ export default class NumberPad extends Vue {
   clear(){
     this.output = '0'
   }
-  // ok(){
-  //
-  // }
+  ok(){
+    this.$emit('update:value', this.output)
+  }
 }
 </script>
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,27 +1,50 @@
 <template>
   <div>
+    {{record}}
     <Layout class-prefix="layout">
-      <NumberPad/>
-      <Types/>
-      <Notes/>
-      <Tags :data-source.sync="tags"/>
+      <NumberPad :value.sync="record.amount"/>
+      <Types :value.sync="record.type"/>
+      <Notes @update:value="onUpdateNotes"/>
+      <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
     </Layout>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-export default {
-  name: "Money",
-  data(){
-    return {
-      tags: ['衣','食','住','行']
-    }
-  },
-  components: {Tags, Notes, Types, NumberPad},
+import {Component} from 'vue-property-decorator';
+
+type Record = {
+  tags: string[]
+  notes: string
+  type: string
+  amount: number
+}
+
+@Component({
+  components: {Tags, Notes, Types, NumberPad}
+})
+export default class Money extends Vue {
+  tags = ['衣', '食', '住', '行'];
+  record: Record = {
+    tags: [],
+    notes: '',
+    type: '-',
+    amount: 0
+  };
+
+  onUpdateTags(value: string[]) {
+    this.record.tags = value
+  }
+
+  onUpdateNotes(value: string) {
+    this.record.notes = value
+  }
+
 }
 </script>
 
