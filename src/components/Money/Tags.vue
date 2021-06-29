33e5058f4b13108d1559dd2d028faded6dd78bf4
xuhao <bingoxuhao@gmail.com>
2021-06-29 10:59:04 +0800

    解决添加标签取消后会新增空标签的 bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -35,6 +35,8 @@ export default class Tags extends Vue {
     const name = window.prompt('请输入标签名');
     if (name === '') {
       window.alert('标签名不能为空');
+    } else if(name === null) {
+      return;
     } else if (this.dataSource) {
       this.$emit('update:dataSource', [...this.dataSource, name]);
     }
