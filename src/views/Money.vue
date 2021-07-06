598009fd878f515ffad232a531c305533f50281d
xuhao <bingoxuhao@gmail.com>
2021-07-06 17:26:36 +0800

    修复 value 被赋值的警告

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div>{{record}}
     <Layout class-prefix="layout">
       <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
       <Types :value.sync="record.type"/>
