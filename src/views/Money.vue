4214026268c0b61d031ee5e43485a4139f7b61ca
xuhao <bingoxuhao@gmail.com>
2021-09-16 21:16:36 +0800

    抽离 Tabs.vue 使用在 Statistics.vue 中

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,7 +20,6 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store/index.ts'
 
 
 @Component({
