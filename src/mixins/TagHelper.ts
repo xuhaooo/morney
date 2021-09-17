e406c4b846c8867905e35792292f7beea4f4a2cd
xuhao <bingoxuhao@gmail.com>
2021-09-17 16:43:15 +0800

    修复标签为空情况下一进页面就弹出 4 个创建标签提示的 bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
+const map: {[key:string]: string} = {
+  'tag name duplicated': '标签名重复了'
+}
+
 @Component
 export class TagHelper extends Vue{
   createTag() {
@@ -11,6 +15,9 @@ export class TagHelper extends Vue{
       return;
     } else{
       this.$store.commit('createTag', name);
+      if(this.$store.state.createTagError){
+          window.alert(map[this.$store.state.createTagError.message] || '未知错误')
+      }
     }
   }
 }
