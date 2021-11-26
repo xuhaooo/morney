5b14f009d84eea63c0528f8d4ea9690d696f1b54
xuhao <bingoxuhao@gmail.com>
2021-11-26 21:28:05 +0800

    完善 Chart.vue 细节样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -57,7 +57,9 @@ export default class Statistics extends Vue {
   }
 
   mounted() {
-    (this.$refs.chartWrapper as HTMLDivElement).scrollLeft = 9999
+    const div = this.$refs.chartWrapper as HTMLDivElement
+    div.scrollLeft = div.scrollWidth
+
   }
 
   get x() {
@@ -74,13 +76,18 @@ export default class Statistics extends Vue {
           '15', '16', '17', '18', '19', '20', '21',
           '22', '23', '24', '25', '26', '27', '28',
           '29', '30'
-        ]
+        ],
+        axisTick: {alignWithLabel: true},
+        axisLine: {lineStyle: {color: '#666'}}
       },
       yAxis: {
         type: 'value',
         show: false
       },
       series: [{
+        symbol: 'circle',
+        symbolSize: 12,
+        itemStyle: {borderWidth: 1, color: '#666', borderColor:'#666'},
         data: [
           820, 932, 901, 934, 1290, 1330, 1320,
           820, 932, 901, 934, 1290, 1330, 1320,
@@ -89,7 +96,12 @@ export default class Statistics extends Vue {
         ],
         type: 'line'
       }],
-      tooltip: {show: true}
+      tooltip: {
+        show: true,
+        triggerOn: 'click',
+        formatter: '{c}',
+        position: 'top'
+      }
     }
   }
 
@@ -184,6 +196,12 @@ export default class Statistics extends Vue {
   width: 428.57%;
   &-wrapper {
     overflow: auto;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    overflow-x: scroll;
+    &::-webkit-scrollbar {
+      display: none;
+    }
   }
 }
 </style>
